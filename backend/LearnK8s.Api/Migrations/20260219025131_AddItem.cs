7b9839d0c4ff9ffb20c8e6a3709c177c9262b87d
kokutaro <h_kohara@me.com>
2026-02-19 11:52:22 +0900

    Add migration to create items table with necessary fields

---
+++ backend/LearnK8s.Api/Migrations/20260219025131_AddItem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LearnK8s.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "items",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    description = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    price = table.Column<decimal>(type: "numeric", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    last_updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    last_updated_by = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_items", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "items");
+        }
+    }
+}
